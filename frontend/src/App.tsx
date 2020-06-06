162a60a8ae06ee64e06f9733f2f5f29c8a700d47
FrancyelZelling <zellingf@gmail.com>
2020-06-06 15:59:55 -0300

    Second Lesson Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
+        <a href="http://www.google.com">Go to google Motherfucker.</a>
       </header>
     </div>
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("listagem de usuários");
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json(["Diego", "Cleiton", "Robson"]);
+app.get("/users", (request, response) => {
+  return response.json({ message: "Hello World" });
 });
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
